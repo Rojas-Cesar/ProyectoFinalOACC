1228715aa6abd57d1ebf85a1b090f1f14a68f474
César Iván Rojas Salazar <72680759+Rojas-Cesar@users.noreply.github.com>
2022-06-09 23:33:01 -0500

    Update Proyecto.sql

--- Proyecto.sql
+++ Proyecto.sql
@@ -1,10 +1,10 @@
+-- CRUD OPERATIONS
+
 show databases;
 create database if not exists oacc2809;
 show databases;
 use oacc2809;
 show tables;
-
--- CRUD OPERATIONS
 
 create table if not exists alumnos(
 	id_alumno int not null auto_increment,
@@ -19,9 +19,44 @@ create table if not exists alumnos(
 show tables;
 show columns from alumnos;
 
-insert into alumnos values('570110', 'GABRIELA', 'MISTRAL', 'ALCAYAGA', '2007-01-10', 'ESTADOUNIDENSE');
-insert into alumnos values('800710', 'MARIO', 'ROJO', 'CHAVEZ', '2007-11-11', 'ESTADOUNIDENSE');
-insert into alumnos values('952001', 'REGINA', 'REYES', 'ROSA', '2007-11-11', 'ESTADOUNIDENSE');
+-- C -> CREATE
+insert into alumnos values('570110', 'GABRIELA', 'MISTRAL', 'ALCAYAGA', '2007-01-01', 'Estadounidense');
+insert into alumnos values('800710', 'MARIO', 'ROJO', 'CHAVEZ', '2007-11-11', 'Francesa');
+insert into alumnos values('952001', 'REGINA', 'REYES', 'ROSA', '2007-12-12', 'Cubana');
+insert into alumnos values('829892', 'PABLO', 'PEREZ', 'PEREZ', '2007-09-09', 'Canadiense');
+insert into alumnos values('293621', 'JORGE', 'JUSTO', 'JUSTO', '2007-08-08', 'Mexicana');
+insert into alumnos values('672927', 'ROGELIO', 'RAMOS', 'ROMAN', '2007-07-07', 'DF');
+insert into alumnos values('102938', '', 'ROJAS', 'SALAZAR', '2007-06-06', 'Mexicana');
+insert into alumnos values('67584', 'JACOBO', 'LOPEZ', 'GOMEZ', '2007-05-05', 'Mexicana');
+
+-- R -> READ
+select * from alumnos;
+
+select nombre NOMBRE, nacionalidad PAIS
+from alumnos
+where nacionalidad <> 'Mexicana';
+
+select id_alumno ID, nombre NOMBRE, nacionalidad PAIS
+from alumnos
+where nombre is null
+or nombre = '';
+
+select count(*) EXTRANJEROS
+from alumnos
+where nacionalidad not in ('Mexicana');
+
+-- U -> UPDATE
+update alumnos set nacionalidad = 'Mexicana'
+where nacionalidad = 'DF';
+
+update alumnos set nombre = trim(nombre);
+
+-- D -> DELETE
+delete from alumnos where trim(nombre) = '';
+
+delete from alumnos where length(id_alumno) <> 6;
 
 
 select * from alumnos;
+
+drop table alumnos;
